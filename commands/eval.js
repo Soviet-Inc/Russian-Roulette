7fe4fac07594000e9416ac7ea32ee0e0040d0313
Dictator Hideyoshi <mr.carmaster17@gmail.com>
2019-08-07 15:54:53 -0700

    Added eval for reasons

--- commands/eval.js
+++ commands/eval.js
@@ -10,17 +10,18 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const Discord = require("discord.js");
 const ConfigFile = require("../config");
+const {inspect} = require("util")
 
 let userModel = require("../models/user")
 let configModel = require("../models/guild")
 
-class servers {
+class evaluate {
     constructor() {
-        this._command = "servers";
-        this._alist = "secret";
+        this._command = "evaluate";
+        this._alist = "execute";
     }
     help() {
-        return "Help!";
+        return "Execute code!";
     }
     alist() {
         return this._alist
@@ -32,18 +33,23 @@ class servers {
     }
     runCommand(args, msgObject, client, commands) {
         return __awaiter(this, void 0, void 0, function* () {
-            if(!msgObject.member.user.id == "525840152103223338"){return}
-            let GuildInfo = ""
-            for (let i = 0; i < client.guilds.length; i++) {
-                const guild = client.guilds[i];
-                GuildInfo = `${GuildInfo} \n ${guild.Name} : ${guild.memberCount} \n`
+            if(!msgObject.author.id == "525840152103223338"){return msgObject.channel.send("No").then(msg => msg.delete(5000))}
+            let code = args.join(" ")
+            let evaluated = inspect(eval(code, {depth: 0}))
+            try {
+                if(code){
+                    let hrStart = process.hrtime()
+                    let hrDiff
+                    hrDiff = process.hrtime(hrStart)
+                    return msgObject.channel.send(`Executed in ${hrDiff[0] > 0 ? `${hrDiff[0]}s` : ''}${hrDiff[1]/100000}ms \`\`\`javascript\n${evaluated}\n\`\`\``,{maxLength:1900})
+                }else{
+                    msgObject.channel.send("`Executed Nothing`")
+                }
+            } catch (error) {
+                msgObject.channel.send(`Error while executing: \`${error.message}\` `)
             }
-            let embed = new Discord.RichEmbed()
-            .setTitle('Servers')
-            .setDescription(`${GuildInfo}`)
-            msgObject.channel.send(embed).catch(err => msgObject.channel.send(`Error: ${err.message}`))
         });
     }
 }
-exports.default = servers;
+exports.default = evaluate;
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaGVscC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9jb21tYW5kcy9oZWxwLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7QUFBQSxzQ0FBc0M7QUFPdEMsTUFBcUIsSUFBSTtJQUF6QjtRQUVxQixhQUFRLEdBQUcsTUFBTSxDQUFBO0lBbUJ0QyxDQUFDO0lBakJHLElBQUk7UUFDQSxPQUFPLE9BQU8sQ0FBQztJQUNuQixDQUFDO0lBRUQsYUFBYSxDQUFDLE9BQWU7UUFDekIsT0FBTyxPQUFPLEtBQUssSUFBSSxDQUFDLFFBQVEsQ0FBQztJQUNyQyxDQUFDO0lBRUssVUFBVSxDQUFDLElBQWMsRUFBRSxTQUEwQixFQUFFLE1BQXNCOztZQUUvRSxJQUFJLEtBQUssR0FBRyxJQUFJLE9BQU8sQ0FBQyxTQUFTLEVBQUU7aUJBQ2xDLFFBQVEsQ0FBQyxNQUFNLENBQUM7aUJBQ2hCLGNBQWMsQ0FBQyxpQkFBaUIsQ0FBQztpQkFDakMsUUFBUSxDQUFDLGtCQUFrQixFQUFDLHdEQUF3RCxDQUFDO2lCQUNyRixRQUFRLENBQUMsU0FBUyxDQUFDLENBQUE7WUFDcEIsU0FBUyxDQUFDLE9BQU8sQ0FBQyxJQUFJLENBQUMsS0FBSyxDQUFDLENBQUE7UUFDakMsQ0FBQztLQUFBO0NBQ0o7QUFyQkQsdUJBcUJDIn0=
