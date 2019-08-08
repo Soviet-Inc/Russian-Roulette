f7e0eaaea72619e0153ae257cc6a9e57191012d8
Dictator Hideyoshi <mr.carmaster17@gmail.com>
2019-08-07 20:01:57 -0700

    ,

--- Manager.js
+++ Manager.js
@@ -1,15 +1,20 @@
 const Discord = require("discord.js");
-const dbl = require("dblapi.js")
-const app = require("express")()
+const dblapi = require("dblapi.js")
+const http = require("http")
 
 const ConfigFile = require("./config")
 
-const client = new Discord.Client();
+const server = http.createServer().listen(process.env.PORT || 3000,"https://russian-roulette-discord-bot.herokuapp.com")
 
-app.post("/vote",(req,res) => {
-    console.log(req.body.type)
-    console.log(req.ip)
-})
+const client = new Discord.Client();
+const dbl = new dblapi(process.env.dblToken,{webhookAuth:process.env.authentication, webhookPath: "/vote", webhookServer:server})
+
+dbl.webhook.on('ready', hook => {
+    console.log(`Webhook running at http://${hook.hostname}:${hook.port}${hook.path}`);
+  });
+  dbl.webhook.on('vote', vote => {
+    console.log(`User with ID ${vote.user} just voted!`);
+});
+
 
 client.login(ConfigFile.config.token);
-app.listen(process.env.PORT || 3000)
