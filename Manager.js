e6042c8deb29fa68aae63dfa768907ffceaeff8a
Dictator Hideyoshi <mr.carmaster17@gmail.com>
2019-08-07 20:17:12 -0700

    I think I got it

--- Manager.js
+++ Manager.js
@@ -11,9 +11,37 @@ const dbl = new dblapi(process.env.dblToken,{webhookAuth:process.env.authenticat
 
 dbl.webhook.on('ready', hook => {
     console.log(`Webhook running at http://${hook.hostname}:${hook.port}${hook.path}`);
-  });
-  dbl.webhook.on('vote', vote => {
-    console.log(`User with ID ${vote.user} just voted!`);
+});
+
+dbl.webhook.on('vote', vote => {
+    client.fetchWebhook("527324432511795208","kQbR4QuXe_XCkTe61mwtfmU2PGfJOtgWqvWQFjA1iGwD6BobXcGcObmSdfRmYi1EMUbU").then(hook => {
+        client.fetchUser(vote.user).then(user => {
+            hook.sendMessage(`${user.username} just voted! They get a mine bribe!`)
+        })
+        userModel.findOne({
+            UserId:user
+        }, (err,user) => {
+            if (err){console.error(err)}
+            if(!user) {
+                let Member = client.fetchUser(user).then(member => {return member.tag})
+                const newUser = new userModel({
+                    UserId: user,
+                    UserTag: Member || "Can't find",
+                    wins: 0,
+                    draws: 0,
+                    loses: 0,
+                    money: 0,
+                    inventory: [
+                        `Mine Bribe`
+                    ]
+                })
+                return newUser.save()
+            }else{
+                user.inventory.push(`Mine Bribe`)
+                user.save()
+            }
+        })
+    })
 });
 
 
