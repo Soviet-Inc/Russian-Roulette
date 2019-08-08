315691b1e168e87e99bc932ae7b44ce1fac8f5a1
Dictator Hideyoshi <mr.carmaster17@gmail.com>
2019-08-07 20:27:02 -0700

    fheisuwgh78sehiusw

--- Manager.js
+++ Manager.js
@@ -19,16 +19,16 @@ dbl.webhook.on('ready', hook => {
 
 dbl.webhook.on('vote', vote => {
     client.fetchWebhook("608819294876336149","kQbR4QuXe_XCkTe61mwtfmU2PGfJOtgWqvWQFjA1iGwD6BobXcGcObmSdfRmYi1EMUbU").then(hook => {
-        client.fetchUser(vote.user).then(user => {
+        client.fetchUser(vote.user).then(User => {
             hook.sendMessage(`${user.username} just voted! They get a mine bribe!`)
             userModel.findOne({
-                UserId:user.id
+                UserId:User.id
             }, (err,user) => {
                 if (err){console.error(err)}
                 if(!user) {
                     const newUser = new userModel({
-                        UserId: user.id,
-                        UserTag: user.username || "Can't find",
+                        UserId: User.id,
+                        UserTag: User.username || "Can't find",
                         wins: 0,
                         draws: 0,
                         loses: 0,
